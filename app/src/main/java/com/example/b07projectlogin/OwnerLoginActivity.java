2c51e9939b2e3d8d356dfc9029cf1ccc01869e44
hiromikoreyes <[200~114784445+hiromikoreyes@users.noreply.github.com>
2023-07-28 18:34:40 -0400

    unzipped file

---
+++ app/src/main/java/com/example/b07projectlogin/OwnerLoginActivity.java
@@ -0,0 +1,14 @@
+package com.example.b07projectlogin;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class OwnerLoginActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.ownerlogin);
+    }
+}
