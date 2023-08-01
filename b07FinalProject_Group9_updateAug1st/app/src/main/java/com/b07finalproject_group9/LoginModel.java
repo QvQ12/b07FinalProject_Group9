9d408c0f51b161f900a3f5eeca094b609761ba24
cristal <wtcristal@cristaldeMacBook-Air.local>
2023-08-01 17:52:33 -0400

    finished login and signup pages

---
+++ b07FinalProject_Group9_updateAug1st/app/src/main/java/com/b07finalproject_group9/LoginModel.java
@@ -0,0 +1,135 @@
+package com.b07finalproject_group9;
+
+import android.util.Log;
+import android.widget.Toast;
+
+import androidx.annotation.NonNull;
+
+import com.b07finalproject_group9.objects.ShopperUser;
+import com.b07finalproject_group9.objects.StoreOwnerUser;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+
+import java.util.concurrent.CompletableFuture;
+
+public class LoginModel extends DatabaseModel{
+
+    private void createNewStoreOwnerUser(String storename, String username, String password){
+        /* Writes a new StoreOwner User to the database
+         */
+        StoreOwnerUser user = new StoreOwnerUser(storename, username, password, "TODO make a function that creates this");
+        DatabaseReference db = fdb.getReference("StoreOwner-UserList");
+        db.child(username).setValue(user.createMap());
+    }
+
+    private void createNewShopperUser(String username, String password){
+        /* Writes a new Shopper User to the database
+         */
+        ShopperUser user = new ShopperUser(username,
+                password,"TODO make a function that creates");
+        DatabaseReference db = fdb.getReference("Shopper-UserList");
+        db.child(username).setValue(user.createMap());
+    }
+    public CompletableFuture<Boolean> signUpShopperUser(String username, String password){
+        /*  Creates a new SHOPPER user iff there is NOT an existing SHOPPER user
+            with the same username, returns a completeablefuture which gives true if completed
+            or false otherwise.
+         */
+        CompletableFuture<Boolean> completableFuture = new CompletableFuture<>();
+        DatabaseReference query = fdb.getReference("Shopper-UserList").child("");
+        query.addListenerForSingleValueEvent(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot snapshot) {
+                if(snapshot.hasChild(username)){
+                    completableFuture.complete(false);
+                } else{
+                    createNewShopperUser(username, password);
+                    completableFuture.complete(true);
+                }
+            }
+            @Override
+            public void onCancelled(@NonNull DatabaseError error) {
+                completableFuture.complete(false);
+            }
+        });
+        return completableFuture;
+    }
+    public CompletableFuture<Boolean> signUpStoreOwner(String storename,
+                                                       String username, String password) {
+        /*  Creates a new STOREOWNER user iff there is NOT an existing STOREOWNER user
+            with the same username OR storename, returns a completeablefuture which returns
+            true if successful false otherwise.
+         */
+        DatabaseReference query = fdb.getReference("StoreOwner-UserList");
+        CompletableFuture<Boolean> completableFuture = new CompletableFuture<>();
+
+        query.addListenerForSingleValueEvent(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot snapshot) {
+                if (snapshot.hasChild(username) || snapshot.hasChild(storename)) {
+                    completableFuture.complete(false);
+
+                } else {
+                    createNewStoreOwnerUser(storename, username, password);
+                    completableFuture.complete(true);
+
+                }
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError error) {
+                completableFuture.complete(false);
+            }
+        });
+        return completableFuture;
+    }
+
+    public CompletableFuture<Boolean> loginShopper(String username, String password) {
+        CompletableFuture<Boolean> completableFuture = new CompletableFuture<>();
+        DatabaseReference query = (FirebaseDatabase.getInstance().
+                getReference("Shopper-UserList/"));
+
+        query.child(username).get().addOnCompleteListener(new OnCompleteListener<DataSnapshot>() {
+            @Override
+            public void onComplete(@NonNull Task<DataSnapshot> task) {
+                boolean result = false;
+                if (task.isSuccessful()) {
+                    DataSnapshot ds = task.getResult();
+                    String passwordFromDb = String.valueOf(ds.child("password").getValue());
+                    result = passwordFromDb.equals(password);
+                }
+                completableFuture.complete(result);
+            }
+        });
+
+        return completableFuture;
+    }
+
+    public CompletableFuture<Boolean> loginOwner(String username, String password) {
+        CompletableFuture<Boolean> completableFuture = new CompletableFuture<>();
+        DatabaseReference query = (FirebaseDatabase.getInstance().
+                getReference("StoreOwner-UserList/"));
+
+        query.child(username).get().addOnCompleteListener(new OnCompleteListener<DataSnapshot>() {
+            @Override
+            public void onComplete(@NonNull Task<DataSnapshot> task) {
+                boolean result = false;
+                if (task.isSuccessful()) {
+                    DataSnapshot ds = task.getResult();
+                    String passwordFromDb = String.valueOf(ds.child("password").getValue());
+                    result = passwordFromDb.equals(password);
+                }
+                completableFuture.complete(result);
+            }
+        });
+
+        return completableFuture;
+    }
+
+
+}
---
+++ b07FinalProject_Group9_updateAug1st/app/src/main/java/com/b07finalproject_group9/login/ShopperLoginFragment.java
@@ -0,0 +1,134 @@
+package com.b07finalproject_group9.login;
+
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentTransaction;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.AdapterView;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Spinner;
+import android.widget.Toast;
+
+import com.b07finalproject_group9.LoginModel;
+import com.b07finalproject_group9.MainActivity;
+import com.b07finalproject_group9.R;
+import com.tempfragments.ShopperDashboard;
+
+public class ShopperLoginFragment extends Fragment {
+    private void setSpinner(View view,int i){
+        Spinner spinner;
+        spinner = (Spinner) view.findViewById(R.id.spinner);
+        if(spinner!=null)spinner.setSelection(i);
+    }
+
+    private void openShopperSignup(){
+        Fragment f = new ShopperSignUpFragment();
+        FragmentTransaction ft = getParentFragmentManager().beginTransaction();
+        ft.replace(R.id.main_login_redirect, f).commit();
+    }
+
+    private void openOwnerLogin(){
+        Fragment f = new OwnerLoginFragment();
+        FragmentTransaction ft = getParentFragmentManager().beginTransaction();
+        ft.replace(R.id.main_login_redirect, f).commit();
+    }
+
+    private void openOwnerSignup(){
+        Fragment f = new OwnerSignUpFragment();
+        FragmentTransaction ft = getParentFragmentManager().beginTransaction();
+        ft.replace(R.id.main_login_redirect, f).commit();
+    }
+
+    private void  openShopperLogin(){
+        Fragment f = new ShopperLoginFragment();
+        FragmentTransaction ft = getParentFragmentManager().beginTransaction();
+        ft.replace(R.id.main_login_redirect, f).commit();
+    }
+    private void performShopperLogin(String username, String password){
+        LoginModel lm = new LoginModel();
+        lm.loginShopper(username, password)
+                .thenAccept(success -> {
+                    if (success) {
+                        Fragment f = new ShopperDashboard();
+                        FragmentTransaction ft = getParentFragmentManager().beginTransaction();
+                        ft.replace(R.id.main_login_redirect, f).commit();
+                    } else {
+                        Toast.makeText(getActivity(), "Login Unsuccessful",
+                                Toast.LENGTH_SHORT).show();
+                    }
+                });
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        View view = inflater.inflate(R.layout.shopperlogin, container, false);
+        setSpinner(view,0);
+        EditText username_editText = view.findViewById(R.id.username_editText);
+        EditText password_editText = view.findViewById(R.id.password_editText);
+        Button btnLogin = view.findViewById(R.id.shopper_login_button);
+        Spinner spinner = view.findViewById(R.id.spinner);
+        Button menu_button = view.findViewById(R.id.menu_button);
+        menu_button.setOnClickListener(new View.OnClickListener(){
+
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(ShopperLoginFragment.this.getActivity(), MainActivity.class);
+                startActivity(intent);
+            }
+        });
+
+        spinner.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+            @Override
+            public void onItemSelected(AdapterView<?> adapterView, View view, int i, long l) {
+                if(spinner.getSelectedItem().toString().contains("shopper signup")){
+                    openShopperSignup();
+                }
+
+                if(spinner.getSelectedItem().toString().contains("owner login")){
+                    openOwnerLogin();
+                }
+
+                if(spinner.getSelectedItem().toString().contains("owner signup")){
+                    openOwnerSignup();
+                }
+
+//                if(spinner.getSelectedItem().toString().contains("shopper login")){
+//                    openShopperLogin();
+//                }
+            }
+
+            @Override
+            public void onNothingSelected(AdapterView<?> adapterView) {
+                //Nothing
+            }
+        });
+
+
+
+
+        //Do Login On Clicking Login
+        btnLogin.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View view){
+                String username = username_editText.getText().toString();
+                String password = password_editText.getText().toString();
+                performShopperLogin(username, password);
+            }
+        });
+
+
+
+
+
+        return view;
+    }
+
+
+}
+
