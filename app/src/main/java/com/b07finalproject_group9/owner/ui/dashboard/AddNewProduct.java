8fdc38ec1eead4cdc4c9ee16f2c12b426c1e2272
AlvinCao88 <alvincao88@gmail.com>
2023-08-09 16:46:19 -0400

    Edited EditProduct

--- app/src/main/java/com/b07finalproject_group9/owner/ui/dashboard/AddNewProduct.java
+++ app/src/main/java/com/b07finalproject_group9/owner/ui/dashboard/AddNewProduct.java
@@ -37,17 +37,37 @@ public class AddNewProduct extends Fragment {
         confirmAdd.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                StoreOwnerInventoryModel sm = new StoreOwnerInventoryModel();
-                String username = MainActivity.currUser.getUsername();
-                String prod_name = String.valueOf(product_name_editText.getText());
-                int quantity = Integer.valueOf(String.valueOf(quantity_editText.getText()));
-                Double price = Double.valueOf(String.valueOf(price_editText.getText()));
-                String description = String.valueOf(description_editText.getText());
-                String key = sm.addProductInventory(username, prod_name, price, quantity, description);
-                Toast.makeText(getActivity(), "Product successfully added with key: " + key,
-                        Toast.LENGTH_SHORT).show();
+                try {
+                    StoreOwnerInventoryModel sm = new StoreOwnerInventoryModel();
+                    String username = MainActivity.currUser.getUsername();
+                    String prod_name = String.valueOf(product_name_editText.getText()).trim();
+
+                    if (prod_name.isEmpty() || quantity_editText.getText().toString().trim().isEmpty()
+                            || price_editText.getText().toString().trim().isEmpty() || description_editText.getText().toString().trim().isEmpty()) {
+                        Toast.makeText(getActivity(), "Please fill in all fields", Toast.LENGTH_SHORT).show();
+                        return;
+                    }
+
+                    int quantity = Integer.valueOf(quantity_editText.getText().toString().trim());
+                    Double price = Double.valueOf(price_editText.getText().toString().trim());
+                    String description = String.valueOf(description_editText.getText()).trim();
+
+                    String key = sm.addProductInventory(username, prod_name, price, quantity, description);
+                    Toast.makeText(getActivity(), "Product successfully added with key: " + key, Toast.LENGTH_SHORT).show();
+
+                    // Navigate to DashboardFragment
+                    FragmentTransaction transaction = getActivity().getSupportFragmentManager().beginTransaction();
+                    transaction.replace(R.id.container, new DashboardFragment()); // Assuming 'container' is the ID of your fragment container
+                    transaction.commit();
+
+                } catch (NumberFormatException e) {
+                    Toast.makeText(getActivity(), "Invalid number format", Toast.LENGTH_SHORT).show();
+                } catch (Exception e) {
+                    Toast.makeText(getActivity(), "Error: " + e.getMessage(), Toast.LENGTH_SHORT).show();
+                }
             }
         });
+
         // Inflate the layout for this fragment
         return view;
     }
