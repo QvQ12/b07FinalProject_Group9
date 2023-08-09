41e6c20d51cc71f67b1d543f5d4a03a7f7ed8bc1
AlvinCao88 <alvincao88@gmail.com>
2023-08-09 15:10:19 -0400

    Status Pages

--- app/src/main/java/com/b07finalproject_group9/shopper/order/StatusPage.java
+++ app/src/main/java/com/b07finalproject_group9/shopper/order/StatusPage.java
@@ -3,64 +3,66 @@ package com.b07finalproject_group9.shopper.order;
 import android.os.Bundle;
 
 import androidx.fragment.app.Fragment;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
+import com.b07finalproject_group9.OrderModel;
 import com.b07finalproject_group9.R;
+import com.b07finalproject_group9.objects.ProductInfo;
+import com.b07finalproject_group9.owner.StoreOwnerInventoryModel;
+import com.b07finalproject_group9.shopper.ShopperModel;
+import com.b07finalproject_group9.shopper.dashboard.ProductShopperAdapter;
 
-/**
- * A simple {@link Fragment} subclass.
- * Use the {@link StatusPage#newInstance} factory method to
- * create an instance of this fragment.
- */
+import java.util.ArrayList;
+import java.util.HashMap;
+
 public class StatusPage extends Fragment {
+    RecyclerView recyclerView;
+    private String key;
+    private StatusAdapter statusAdapter;
+    private ArrayList<ProductInfo> productList = new ArrayList<>();
+    private OrderModel om = new OrderModel();
+    private ShopperModel shm = new ShopperModel();
+    private StoreOwnerInventoryModel sm = new StoreOwnerInventoryModel();
 
-    // TODO: Rename parameter arguments, choose names that match
-    // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-    private static final String ARG_PARAM1 = "param1";
-    private static final String ARG_PARAM2 = "param2";
+    public StatusPage() { }
 
-    // TODO: Rename and change types of parameters
-    private String mParam1;
-    private String mParam2;
-
-    public StatusPage() {
-        // Required empty public constructor
+    public StatusPage(String key){
+        this.key = key;
     }
 
-    /**
-     * Use this factory method to create a new instance of
-     * this fragment using the provided parameters.
-     *
-     * @param param1 Parameter 1.
-     * @param param2 Parameter 2.
-     * @return A new instance of fragment StatusPage.
-     */
-    // TODO: Rename and change types and number of parameters
-    public static StatusPage newInstance(String param1, String param2) {
-        StatusPage fragment = new StatusPage();
-        Bundle args = new Bundle();
-        args.putString(ARG_PARAM1, param1);
-        args.putString(ARG_PARAM2, param2);
-        fragment.setArguments(args);
-        return fragment;
-    }
 
-    @Override
-    public void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        if (getArguments() != null) {
-            mParam1 = getArguments().getString(ARG_PARAM1);
-            mParam2 = getArguments().getString(ARG_PARAM2);
+    private void processProductID(String prodID, String store){
+        sm.getSpecificProduct(prodID, store).thenAccept(map -> addToProductList(map, prodID));
+    }
+    private void addToProductList(HashMap<String, String> map, String prodID){
+        ProductInfo p = new ProductInfo(map, prodID);
+        productList.add(p);
+        recyclerView.setAdapter(statusAdapter);
+    }
+    private void processListOfProductID(ArrayList<String> list){
+        if(list == null) return;
+        for(int i = 0; i < list.size(); i++){
+            String prodID = list.get(i);
+            shm.getStoreBasedOnProductID(list.get(i))
+                    .thenAccept(store->processProductID(prodID, store));
         }
     }
-
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_status_page, container, false);
+        View view =  inflater.inflate(R.layout.fragment_status_page, container, false);
+        recyclerView = view.findViewById(R.id.orderProductDisplay);
+        recyclerView.setHasFixedSize(true);
+        recyclerView.setLayoutManager(new LinearLayoutManager(view.getContext()));
+        productList = new ArrayList<>();
+        statusAdapter = new StatusAdapter(getContext(), productList,getActivity().getSupportFragmentManager());
+        om.getProductIDbyOrder(key).thenAccept(res -> processListOfProductID(res));
+        return view;
     }
 }
